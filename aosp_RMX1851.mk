2d7ad1f9047da26b797045dbebe013947bad518e
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-03-01 19:13:53 +0530

    Enabled face unlock flag

--- aosp_RMX1851.mk
+++ aosp_RMX1851.mk
@@ -16,6 +16,7 @@
 
 # Specify arch for gapps
 TARGET_GAPPS_ARCH=arm64
+TARGET_USES_FACE_UNLOCK := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
